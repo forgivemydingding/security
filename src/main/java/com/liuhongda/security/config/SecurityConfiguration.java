2544626c17df5f155f83e1a5602cf727f1db555c
liu.hongda <liu.hongda@trs.com.cn>
2019-07-22 17:09:03 +0800

    security study

--- src/main/java/com/liuhongda/security/config/SecurityConfiguration.java
+++ src/main/java/com/liuhongda/security/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.liuhongda.security.config;
 
 import com.liuhongda.security.component.MyUserDetailsService;
+import com.liuhongda.security.handler.MyAuthenticationSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/liuhongda/security/handler/MyAuthenticationSuccessHandler.java
+++ src/main/java/com/liuhongda/security/handler/MyAuthenticationSuccessHandler.java
@@ -1,4 +1,4 @@
-package com.liuhongda.security.config;
+package com.liuhongda.security.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
